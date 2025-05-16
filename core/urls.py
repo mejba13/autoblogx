3dc1bf9cecacc55af12697b235793592f230aacf
mejba.13 <mejba.13@gmail.com>
2025-05-16 12:44:35 +0600

    fixed the url pattern issue

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import GeneratePostAPIView
 
-urlpatterns += [
+urlpatterns= [
     path('generate-post/', GeneratePostAPIView.as_view(), name='generate-post'),
 ]
